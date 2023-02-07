cebe4d700f4e290e8340936296c1ca99f7967d47
Joseph Kim <jkim5@goldengate.cs.brynmawr.edu>
2023-02-07 00:37:55 -0500

    A03 complete

--- A03/dynamic_songs.c
+++ A03/dynamic_songs.c
@@ -1,10 +1,145 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Joseph Kim 
+ * Date: Feb 7th, 2023
+ * Description: This program reads a csv file containing information of songs
+ * and then stores them in an array. It then prints out all the information.
  ---------------------------------------------*/
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// struct for saving all the informaiton of a song
+struct song {
+  char title[128];
+  char artist[128];
+  int duration;
+  float dance;
+  float energy;
+  float tempo;
+  float valence;
+};
+
+// forward declaration of functions
+struct song split(char* line);
+void playlistInfo(struct song playlist[], int size); 
 
 int main() {
+  printf("Welcome to Dynamic Donna's Danceability Directory.\n");
+
+  FILE* infile; // file pointer
+  int buff_len = 300; // size of the below string
+  char* buff; // string that will contain each line of the file
+  buff = malloc((sizeof(char) * buff_len));
+
+  // checking for malloc error
+  if (buff == NULL) {
+    exit(0);
+  }
+
+  // accessing the csv file containing all the songs
+  infile = fopen("songlist.csv", "r");
+
+  // checking for malloc error
+  if (infile == NULL) {
+    exit(0);
+  }
+
+  // getting the number of songs in the csv file
+  fgets(buff, buff_len, infile);
+  int size = atoi(strtok(buff, ","));
+
+  // to skip the line containing headers
+  fgets(buff, buff_len, infile);
+
+  // array of struct song that will store all the information
+  struct song* playlist;
+  playlist = malloc(sizeof(struct song) * size);
+
+  // checking for malloc error
+  if (playlist == NULL) {
+    exit(0);
+  }
+
+  // takes in each line of the file and then stores it in an array of struct song
+  for (int i = 0; i < size; i++) {
+    fgets(buff, buff_len, infile);
+    playlist[i] = split(buff);
+  }
+
+  // prints out all the information of the songs
+  playlistInfo(playlist, size);
+
+  // closing/freeing pointers to prevent memory leak
+  fclose(infile);
+  infile = NULL;
+
+  free(buff);
+  buff = NULL;
+
+  free(playlist);
+  playlist = NULL;
+
   return 0;
+}
+
+/**
+ * takes in a string that contains information of a song and puts
+ * them in the correct fields of a struct song
+ *
+ * @param buff: string that contains all the info
+ * @return: struct song containing all the info
+ *
+ */
+struct song split(char* buff) {
+  struct song temp; // creates a temporary struct
+
+  // puts all the info into appropriate fields, converting the string
+  // into integer or float if necessary
+  strcpy(temp.title, strtok(buff, ","));
+  strcpy(temp.artist, strtok(NULL, ","));
+  temp.duration = atoi(strtok(NULL, ","));
+  temp.dance = atof(strtok(NULL, ","));
+  temp.energy = atof(strtok(NULL, ","));
+  temp.tempo = atof(strtok(NULL, ","));
+  temp.valence = atof(strtok(NULL, ","));
+  return temp;
+}
+
+/**
+ * prints out all the songs in an array in a clean way
+ *
+ * @param playlist[]: array that holds all the songs
+ * @param size: int holding size of array
+ * @return: does not return anything
+ *
+ */
+void playlistInfo(struct song playlist[], int size) {
+  printf("\n");
+
+  // loops through each of the songs in the array
+  for (int i = 0; i < size; i++) {
+
+    // converts the duration of song from milliseconds to minutes:seconds
+    int seconds = playlist[i].duration / 1000;
+    int minutes = 0;
+    while (seconds >= 60) {
+      minutes++;
+      seconds -= 60;
+    }
+
+    // checks if the current song index is greater than 10 for clean formatting
+    if (i >= 10) {
+      printf("%i)  %-24s ", i, playlist[i].title);
+    }
+    else {
+      printf("%i)  %-25s ", i, playlist[i].title);
+    }
+
+    // prints rest of the information of a song
+    printf("artist: %-25s duration: %i:%02d  D: %.3f",
+      playlist[i].artist, minutes, seconds, playlist[i].dance);
+
+    printf("  E: %.3f  T: %.3f  V: %.3f\n", playlist[i].energy,
+      playlist[i].tempo, playlist[i].valence);
+  }
 }
--- A03/slow.c
+++ A03/slow.c
@@ -1,10 +1,69 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Joseph Kim
+ * Date: Feb 6th, 2023 
+ * Description: This prject takes in a string and converts it into an ent-phrase
+ * where there are '.' characters between each letter
  ---------------------------------------------*/
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
+
+  // getting the number of '.'s that will follow each letter
+  printf("Pause length: ");
+  int p_len;
+  scanf("%i", &p_len);
+
+  // getting the string that will turn into ent-phrase
+  printf("Text: ");
+  char buff[32];
+  scanf("%s", buff);
+  int t_len = strlen(buff); // int containing length of input string
+
+  // string that will contain the dots that will come after each letter
+  char* dots = NULL;
+  dots = malloc(sizeof(char) * (p_len+1));
+
+  // checking for malloc error
+  if (dots == NULL) {
+    printf("Bad malloc error\n");
+    exit(0);
+  }
+
+  // putting the given number of dots to a string
+  for (int i = 0; i < p_len; i++) {
+    dots[i] = '.';
+  }
+  dots[p_len] = '\0';
+
+  // string that will contain the ent phrase with sufficient memory
+  char* ent = NULL;
+  ent = malloc(sizeof(char) * (t_len * (p_len + 1) + 1));
+
+  // checking for malloc error
+  if (ent == NULL) {
+    printf("Bad malloc error\n");
+    exit(0);
+  }
+
+  // forming the ent phrase by putting in letter and the dots
+  for (int j = 0; j < t_len; j++) {
+    ent[j * (p_len + 1)] = buff[j];
+    strcat(ent, dots);
+  }
+  ent[t_len * (p_len + 1)] = '\0';
+
+  // printing the ent phrase
+  printf("%s\n", ent);
+
+  // freeing all the pointers to avoid memory leak
+  free(dots);
+  dots = NULL;
+
+  free(ent);
+  ent = NULL;
+
   return 0;
 }
+
